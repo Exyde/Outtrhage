0773888e359e6eaff632aa1a6897dea4d9b316bd
gpiganeau <gregoirepiganeau@hotmail.fr>
2025-12-01 22:39:10 +0900

    update

--- Source/Outtrhage/PlayerCharacter.h
+++ Source/Outtrhage/PlayerCharacter.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "AttackStrategy.h"
 #include "PlayerCharacter.generated.h"
 
 UCLASS()
@@ -19,9 +20,17 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	IAttackStrategy* AttackStrategy1;
+	IAttackStrategy* AttackStrategy2;
+	IAttackStrategy* AttackStrategy3;
+	IAttackStrategy* AttackStrategy4;
+	IAttackStrategy* AttackStrategy5;
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+
+	virtual FVector GetAttackDirection(bool bIsRightStick = true, bool bIsRaw = false);
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
